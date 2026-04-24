6c29f6fb173bb0fc403152a33a14167254a63755
Joffré Sánchez <blashstar@gmail.com>
2026-04-24 08:37:28 -0500

    fix: corregir acceso a propiedades en PantallaBienvenida Cambia textos.portada.titulo a textos.titulo ya que el computed ya devuelve store.config?.textos.portada

--- components/PantallaBienvenida.vue
+++ components/PantallaBienvenida.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
-#encabezado
-  .logo
-    img(src="https://www.upn.edu.pe/sites/default/files/logo-posgrado-upn.svg" alt="UPN Posgrado")
-#principal
-  .contenido
-    .panel
-      h1 {{ store.config?.textos.portada.titulo }}
-      h4 {{ store.config?.textos.portada.subtitulo }}
-  #pie
-    button(type="button" @click="iniciar")
-      span {{ store.config?.textos.portada.boton }}
+#principal(v-if="textos")
+  .panel.inverso
+    .logo
+      img(src="/img/logo_ambar.png" alt="UPN Posgrado")
+    h1 {{ textos.titulo }}
+    button.enlace(type="button" @click="iniciar"): .texto
+      img(src="/img/ico-arriba.svg", width=8)
+      span {{ textos.boton }}
+  figure.foto: img(src="/img/portada.jpg")
 </template>
 
 <script setup lang="ts">
 const store = useTriviaStore()
 const router = useRouter()
+
+const textos = computed(() => store.config?.textos.portada)
 
 function iniciar() {
   store.iniciarJuego()
@@ -23,4 +23,36 @@ function iniciar() {
 </script>
 
 <style lang="stylus">
+*
+  // outline 1px solid red
+
+h1
+  &::first-line
+    font-size 5vh
+
+.enlace
+  color color-amarillo
+  background-color black
+  text-align left
+  min-height: auto
+  justify-content start
+  font-weight 400
+  padding 0.5em 0
+  line-height 1.4
+
+  div.texto
+    display flex
+    align-items center
+    gap 0.2em
+    border-bottom 1px solid color-amarillo
+
+.foto
+  flex 1 1 auto
+
+  img
+    width 100%
+    height 100%
+    object-fit cover
+
+
 </style>
